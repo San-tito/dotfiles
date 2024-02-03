22a61aea3ea6b4c44765f744d9ff03ab8d369029
Santito <68120901+San-tito@users.noreply.github.com>
2024-02-03 18:27:33 +0100

    Update install.sh

--- install.sh
+++ install.sh
@@ -2,33 +2,33 @@
 
 # Dotfiles Santito Install Script
 
-# ANSI Color Variables
-GREEN='\033[1;32m'
-YELLOW='\033[1;33m'
-BLUE='\033[1;34m'
-RED='\033[1;31m'
+# ANSI color variables
+HEADER='\033[1;34m'
+SUCCESS='\033[1;32m'
+WARNING='\033[1;33m'
+ERROR='\033[1;31m'
 RESET='\033[0m'
 
 # Header
-echo -e "\n${BLUE}Dotfiles Santito Install Script${RESET}\n"
+printf "\n${HEADER}Dotfiles Santito Install Script${RESET}\n"
 
 # Clone dotfiles repository with submodules
-echo -e "${GREEN}Cloning dotfiles repository with submodules...${RESET}"
+printf "${SUCCESS}Cloning dotfiles repository with submodules...${RESET}\n"
 git clone --recurse-submodules https://github.com/San-tito/dotfiles
 
 # Ask for confirmation to copy everything
-read -p $'\033[1;33mDo you want to copy all dotfiles to your home directory? (y/n): \033[0m' -n 1 -r
+read -p "$(printf "${WARNING}Do you want to copy all dotfiles to your home directory? (y/n): ${RESET}")" -n 1 -r
 echo    # Move to a new line after user input
 
 if [[ $REPLY =~ ^[Yy]$ ]]; then
     # Copy dotfiles content to home directory
-    echo -e "${GREEN}Copying dotfiles content to home directory...${RESET}"
+    printf "${SUCCESS}Copying dotfiles content to home directory...${RESET}\n"
     cp -r dotfiles/* ~
 
     # Optionally, remove the cloned repository if desired
     # rm -rf dotfiles
 
-    echo -e "${GREEN}Dotfiles installed successfully!${RESET}"
+    printf "${SUCCESS}Dotfiles installed successfully!${RESET}\n"
 else
-    echo -e "${RED}Installation canceled. No files were copied.${RESET}"
+    printf "${ERROR}Installation canceled. No files were copied.${RESET}\n"
 fi
