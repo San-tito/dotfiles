95f857085bed3a6ecf0175708ebb5f7838cba78d
Santito <68120901+San-tito@users.noreply.github.com>
2024-04-18 23:22:55 +0200

    Update init.lua🐹

--- .config/nvim/lua/plugins/init.lua
+++ .config/nvim/lua/plugins/init.lua
@@ -1,14 +1,45 @@
 -- [[ Plugins ]]
 return {
-'tpope/vim-sleuth',
-'navarasu/onedark.nvim',
-{
-    'folke/tokyonight.nvim',
+  'tpope/vim-sleuth',
+  {
+    'neovim/nvim-lspconfig',
+    dependencies = {
+      'williamboman/mason.nvim',
+      'williamboman/mason-lspconfig.nvim',
+      'WhoIsSethDaniel/mason-tool-installer.nvim',
+      { 'j-hui/fidget.nvim', opts = {} },
+      { 'folke/neodev.nvim', opts = {} },
+    },
+  },
+  { 
+    'navarasu/onedark.nvim',
     priority = 1000, 
     init = function()
-      -- Load the colorscheme here.
-      -- any other, such as 'tokyonight-storm', 'tokyonight-moon', or 'tokyonight-day'.
-      vim.cmd.colorscheme 'tokyonight-night'
+      vim.cmd.colorscheme 'onedark'
+    end,
+    opts = {
+        transparent = true,
+        styles = {
+           sidebars = "transparent",
+           floats = "transparent",
+        },
+    }
+  },
+  { 
+    'nvim-treesitter/nvim-treesitter',
+    build = ':TSUpdate',
+    opts = {
+      ensure_installed = { 'bash', 'c', 'html', 'lua', 'luadoc', 'markdown', 'vim', 'vimdoc' },
+      auto_install = true,
+      highlight = {
+        enable = true,
+        additional_vim_regex_highlighting = { 'ruby' },
+      },
+      indent = { enable = true, disable = { 'ruby' } },
+    },
+    config = function(_, opts)
+      require('nvim-treesitter.install').prefer_git = true
+      require('nvim-treesitter.configs').setup(opts)
     end,
   },
 }
